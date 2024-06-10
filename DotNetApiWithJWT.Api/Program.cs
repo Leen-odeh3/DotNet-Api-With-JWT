9ddf8c28771df3cc211dbb4ac9dd5fa685ff9b16
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 01:11:25 +0300

    map between class jwt & data

--- DotNetApiWithJWT.Api/Program.cs
+++ DotNetApiWithJWT.Api/Program.cs
@@ -1,3 +1,6 @@
+using DotNetApiWithJWT.Api.Helper;
+using System.Configuration;
+
 namespace DotNetApiWithJWT.Api
 {
     public class Program
@@ -12,6 +15,8 @@ namespace DotNetApiWithJWT.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
 
             var app = builder.Build();
 
