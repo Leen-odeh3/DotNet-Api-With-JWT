1dad60e45e58920b828db56d4da7065de9d07fc2
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 01:15:53 +0300

    Create User Model & Context

---
+++ DotNetApiWithJWT.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DotNetApiWithJWT.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DotNetApiWithJWT.Api.Data;
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
